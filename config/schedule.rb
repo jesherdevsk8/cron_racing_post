2d5a1ce5fa3c81f2b441030693f4f45e50e2d9aa
Jesher Minelli <jesherdevsk8@gmail.com>
2024-10-20 18:45:28 -0300

    update scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,8 +9,11 @@ APP_ROOT = File.expand_path('..', __dir__)
 env :TZ, 'America/Sao_Paulo'
 
 # Rodar rotinas todos os dias às 23:00
+# grep cron /var/log/syslog
+# tail -f /var/log/syslog
 every 1.day, at: '23:00' do
-  command "rm -f #{APP_ROOT}/tmp/cron.log"
-  command "bundle exec ruby #{APP_ROOT}/bin/run_next_race.rb 2> #{APP_ROOT}/tmp/cron.log"
-  command "bundle exec ruby #{APP_ROOT}/bin/run_last_race.rb 2>> #{APP_ROOT}/tmp/cron.log"
+  command "cd #{APP_ROOT}"
+  command 'rm -f tmp/cron.log'
+  command 'bundle exec ruby bin/run_next_race.rb 2> tmp/cron.log'
+  command 'bundle exec ruby bin/run_last_race.rb 2>> tmp/cron.log'
 end
